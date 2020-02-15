7cd083d2d691a718e24d636160bad805a9f47e43
Daniel <danielrodak27@gmail.com>
2020-02-15 16:27:55 +0100

    Activity displays Currencies fragment now

--- app/src/main/java/pl/denathan/currlator/MainActivity.kt
+++ app/src/main/java/pl/denathan/currlator/MainActivity.kt
@@ -2,11 +2,27 @@ package pl.denathan.currlator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.fragmentContainer
+import pl.denathan.currlator.currencies.CurrenciesFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CurrenciesFragment().display(supportFragmentManager, R.id.fragmentContainer, FRAGMENT_CURRENCIES_TAG)
+    }
+
+    private fun Fragment.display(supportFragmentManager: FragmentManager, containerViewId: Int, fragmentTag: String) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(containerViewId, this, fragmentTag)
+        fragmentTransaction.commit()
+    }
+
+    private companion object {
+        const val FRAGMENT_CURRENCIES_TAG = "CURRENCIES_TAG"
     }
 }
--- app/src/main/java/pl/denathan/currlator/currencies/CurrenciesFragment.kt
+++ app/src/main/java/pl/denathan/currlator/currencies/CurrenciesFragment.kt
@@ -1,10 +1,11 @@
-package pl.denathan.currlator
+package pl.denathan.currlator.currencies
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import pl.denathan.currlator.R
 
 class CurrenciesFragment : Fragment() {
 
