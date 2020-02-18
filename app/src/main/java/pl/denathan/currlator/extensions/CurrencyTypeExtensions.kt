1e6caeaef9e5a79a1cdb389c29ff6a471f8387f0
Daniel <danielrodak27@gmail.com>
2020-02-18 19:29:19 +0100

    Extracted mappers to extension functions

---
+++ app/src/main/java/pl/denathan/currlator/extensions/CurrencyTypeExtensions.kt
@@ -0,0 +1,170 @@
+package pl.denathan.currlator.extensions
+
+import pl.denathan.currlator.R.drawable.australia_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.brazil_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.bulgaria_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.canada_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.china_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.croatia_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.czech_republic_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.denmark_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.euro_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.hong_kong_square_icon_64
+import pl.denathan.currlator.R.drawable.hungary_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.iceland_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.india_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.indonesia_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.israel_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.japan_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.malaysia_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.mexico_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.new_zealand_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.norway_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.philippines_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.poland_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.romania_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.russia_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.singapore_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.south_africa_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.sweden_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.switzerland_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.thailand_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.united_kingdom_flag_square_icon_64
+import pl.denathan.currlator.R.drawable.united_states_of_america_flag_square_icon_64
+import pl.denathan.currlator.R.string.currency_australian_dollar
+import pl.denathan.currlator.R.string.currency_brazilian_real
+import pl.denathan.currlator.R.string.currency_british_pound
+import pl.denathan.currlator.R.string.currency_bulgarian_lev
+import pl.denathan.currlator.R.string.currency_canadian_dollar
+import pl.denathan.currlator.R.string.currency_chinese_juan
+import pl.denathan.currlator.R.string.currency_croatian_kuna
+import pl.denathan.currlator.R.string.currency_czech_koruna
+import pl.denathan.currlator.R.string.currency_danish_krone
+import pl.denathan.currlator.R.string.currency_euro
+import pl.denathan.currlator.R.string.currency_hong_kong_dollar
+import pl.denathan.currlator.R.string.currency_hungarian_fornit
+import pl.denathan.currlator.R.string.currency_icelandic_krona
+import pl.denathan.currlator.R.string.currency_indian_rupee
+import pl.denathan.currlator.R.string.currency_indonesian_rupiah
+import pl.denathan.currlator.R.string.currency_isreali_new_shekel
+import pl.denathan.currlator.R.string.currency_japanese_yen
+import pl.denathan.currlator.R.string.currency_malaysian_ringgit
+import pl.denathan.currlator.R.string.currency_mexican_peso
+import pl.denathan.currlator.R.string.currency_new_zealand_dollar
+import pl.denathan.currlator.R.string.currency_norwegian_krone
+import pl.denathan.currlator.R.string.currency_philippine_peso
+import pl.denathan.currlator.R.string.currency_polish_zloty
+import pl.denathan.currlator.R.string.currency_romanian_leu
+import pl.denathan.currlator.R.string.currency_russian_ruble
+import pl.denathan.currlator.R.string.currency_singapore_dollar
+import pl.denathan.currlator.R.string.currency_south_african_rand
+import pl.denathan.currlator.R.string.currency_south_korean_won
+import pl.denathan.currlator.R.string.currency_swedish_krona
+import pl.denathan.currlator.R.string.currency_swiss_franc
+import pl.denathan.currlator.R.string.currency_thai_baht
+import pl.denathan.currlator.R.string.currency_united_states_dollar
+import pl.denathan.currlator.remote.data.CurrencyType
+import pl.denathan.currlator.remote.data.CurrencyType.AUSTRALIAN_DOLLAR
+import pl.denathan.currlator.remote.data.CurrencyType.BRAZILIAN_REAL
+import pl.denathan.currlator.remote.data.CurrencyType.BRITISH_POUND
+import pl.denathan.currlator.remote.data.CurrencyType.BULGARIAN_LEV
+import pl.denathan.currlator.remote.data.CurrencyType.CANADIAN_DOLLAR
+import pl.denathan.currlator.remote.data.CurrencyType.CHINESE_JUAN
+import pl.denathan.currlator.remote.data.CurrencyType.CROATIAN_KUNA
+import pl.denathan.currlator.remote.data.CurrencyType.CZECH_KORUNA
+import pl.denathan.currlator.remote.data.CurrencyType.DANISH_KRONE
+import pl.denathan.currlator.remote.data.CurrencyType.EURO
+import pl.denathan.currlator.remote.data.CurrencyType.HONG_KONG_DOLLAR
+import pl.denathan.currlator.remote.data.CurrencyType.HUNGARIAN_FORNIT
+import pl.denathan.currlator.remote.data.CurrencyType.ICELANDIC_KRONA
+import pl.denathan.currlator.remote.data.CurrencyType.INDIAN_RUPEE
+import pl.denathan.currlator.remote.data.CurrencyType.INDONESIAN_RUPIAH
+import pl.denathan.currlator.remote.data.CurrencyType.ISRAELI_NEW_SHEKEL
+import pl.denathan.currlator.remote.data.CurrencyType.JAPANESE_YEN
+import pl.denathan.currlator.remote.data.CurrencyType.MALAYSIAN_RINGGIT
+import pl.denathan.currlator.remote.data.CurrencyType.MEXICAN_PESO
+import pl.denathan.currlator.remote.data.CurrencyType.NEW_ZEALAND_DOLLAR
+import pl.denathan.currlator.remote.data.CurrencyType.NORWEGIAN_KRONE
+import pl.denathan.currlator.remote.data.CurrencyType.PHILIPPINE_PESO
+import pl.denathan.currlator.remote.data.CurrencyType.POLISH_ZLOTY
+import pl.denathan.currlator.remote.data.CurrencyType.ROMANIAN_LEU
+import pl.denathan.currlator.remote.data.CurrencyType.RUSSIAN_RUBLE
+import pl.denathan.currlator.remote.data.CurrencyType.SINGAPORE_DOLLAR
+import pl.denathan.currlator.remote.data.CurrencyType.SOUTH_AFRICAN_RAND
+import pl.denathan.currlator.remote.data.CurrencyType.SOUTH_KOREAN_WON
+import pl.denathan.currlator.remote.data.CurrencyType.SWEDISH_KRONA
+import pl.denathan.currlator.remote.data.CurrencyType.SWISS_FRANC
+import pl.denathan.currlator.remote.data.CurrencyType.THAI_BAHT
+import pl.denathan.currlator.remote.data.CurrencyType.UNITED_STATES_DOLLAR
+
+fun CurrencyType.findCurrencyIcon(): Int =
+    when (this) {
+        EURO -> euro_flag_square_icon_64
+        AUSTRALIAN_DOLLAR -> australia_flag_square_icon_64
+        BULGARIAN_LEV -> bulgaria_flag_square_icon_64
+        BRAZILIAN_REAL -> brazil_flag_square_icon_64
+        CANADIAN_DOLLAR -> canada_flag_square_icon_64
+        SWISS_FRANC -> switzerland_flag_square_icon_64
+        CHINESE_JUAN -> china_flag_square_icon_64
+        CZECH_KORUNA -> czech_republic_flag_square_icon_64
+        DANISH_KRONE -> denmark_flag_square_icon_64
+        BRITISH_POUND -> united_kingdom_flag_square_icon_64
+        HONG_KONG_DOLLAR -> hong_kong_square_icon_64
+        CROATIAN_KUNA -> croatia_flag_square_icon_64
+        HUNGARIAN_FORNIT -> hungary_flag_square_icon_64
+        INDONESIAN_RUPIAH -> indonesia_flag_square_icon_64
+        ISRAELI_NEW_SHEKEL -> israel_flag_square_icon_64
+        INDIAN_RUPEE -> india_flag_square_icon_64
+        ICELANDIC_KRONA -> iceland_flag_square_icon_64
+        JAPANESE_YEN -> japan_flag_square_icon_64
+        SOUTH_KOREAN_WON -> south_africa_flag_square_icon_64
+        MEXICAN_PESO -> mexico_flag_square_icon_64
+        MALAYSIAN_RINGGIT -> malaysia_flag_square_icon_64
+        NORWEGIAN_KRONE -> norway_flag_square_icon_64
+        NEW_ZEALAND_DOLLAR -> new_zealand_flag_square_icon_64
+        PHILIPPINE_PESO -> philippines_flag_square_icon_64
+        POLISH_ZLOTY -> poland_flag_square_icon_64
+        ROMANIAN_LEU -> romania_flag_square_icon_64
+        RUSSIAN_RUBLE -> russia_flag_square_icon_64
+        SWEDISH_KRONA -> sweden_flag_square_icon_64
+        SINGAPORE_DOLLAR -> singapore_flag_square_icon_64
+        THAI_BAHT -> thailand_flag_square_icon_64
+        UNITED_STATES_DOLLAR -> united_states_of_america_flag_square_icon_64
+        SOUTH_AFRICAN_RAND -> south_africa_flag_square_icon_64
+    }
+
+fun CurrencyType.findCurrencyFullNameId(): Int =
+    when (this) {
+        EURO -> currency_euro
+        AUSTRALIAN_DOLLAR -> currency_australian_dollar
+        BULGARIAN_LEV -> currency_bulgarian_lev
+        BRAZILIAN_REAL -> currency_brazilian_real
+        CANADIAN_DOLLAR -> currency_canadian_dollar
+        SWISS_FRANC -> currency_swiss_franc
+        CHINESE_JUAN -> currency_chinese_juan
+        CZECH_KORUNA -> currency_czech_koruna
+        DANISH_KRONE -> currency_danish_krone
+        BRITISH_POUND -> currency_british_pound
+        HONG_KONG_DOLLAR -> currency_hong_kong_dollar
+        CROATIAN_KUNA -> currency_croatian_kuna
+        HUNGARIAN_FORNIT -> currency_hungarian_fornit
+        INDONESIAN_RUPIAH -> currency_indonesian_rupiah
+        ISRAELI_NEW_SHEKEL -> currency_isreali_new_shekel
+        INDIAN_RUPEE -> currency_indian_rupee
+        ICELANDIC_KRONA -> currency_icelandic_krona
+        JAPANESE_YEN -> currency_japanese_yen
+        SOUTH_KOREAN_WON -> currency_south_korean_won
+        MEXICAN_PESO -> currency_mexican_peso
+        MALAYSIAN_RINGGIT -> currency_malaysian_ringgit
+        NORWEGIAN_KRONE -> currency_norwegian_krone
+        NEW_ZEALAND_DOLLAR -> currency_new_zealand_dollar
+        PHILIPPINE_PESO -> currency_philippine_peso
+        POLISH_ZLOTY -> currency_polish_zloty
+        ROMANIAN_LEU -> currency_romanian_leu
+        RUSSIAN_RUBLE -> currency_russian_ruble
+        SWEDISH_KRONA -> currency_swedish_krona
+        SINGAPORE_DOLLAR -> currency_singapore_dollar
+        THAI_BAHT -> currency_thai_baht
+        UNITED_STATES_DOLLAR -> currency_united_states_dollar
+        SOUTH_AFRICAN_RAND -> currency_south_african_rand
+    }
