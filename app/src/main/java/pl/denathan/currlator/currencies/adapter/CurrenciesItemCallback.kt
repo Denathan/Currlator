ef3eb5a763fa87f1417afbe7f577f582f5ba98b5
Daniel <danielrodak27@gmail.com>
2020-02-20 18:32:08 +0100

    Made Euro to be also updated

--- app/src/main/java/pl/denathan/currlator/currencies/adapter/CurrenciesItemCallback.kt
+++ app/src/main/java/pl/denathan/currlator/currencies/adapter/CurrenciesItemCallback.kt
@@ -7,8 +7,7 @@ class CurrenciesItemCallback : DiffUtil.ItemCallback<Currency>() {
     override fun areItemsTheSame(oldItem: Currency, newItem: Currency): Boolean =
         oldItem.currencyType == oldItem.currencyType
 
-    override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean =
-        oldItem.rate == newItem.rate
+    override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean = false
 
     override fun getChangePayload(oldItem: Currency, newItem: Currency): Any? =
         oldItem.rate == newItem.rate
